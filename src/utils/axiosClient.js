5c1c18a29077b12e141479abe763be38cd939844
saed abbas <saedabbas0@gmail.com>
2025-04-03 15:06:55 +0300

    last changes

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiUrl = process.env.STRAPI_API_URL; 
+const apiUrl = "http://localhost:1337";
 
 const axiosClient = axios.create({
   baseURL: apiUrl,
