d107757924745fdd1c988630265f1a6b99818ee8
saed abbas <saedabbas0@gmail.com>
2025-04-05 16:39:16 +0300

    last changes

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,43 +1,20 @@
-import fetch from 'node-fetch';
-import { serialize } from 'cookie';
+import { parse } from "cookie";
 
-export async function POST(req, res) {
-  if (req.method === 'POST') {
-    const { email, password } = await req.json();  // استلام البريد وكلمة المرور
+export async function getServerSideProps({ req }) {
+  const cookies = parse(req.headers.cookie || "");
+  const token = cookies.token;
 
-    try {
-      // إرسال الطلب إلى Strapi لتسجيل الدخول
-      const response = await fetch(`${process.env.STRAPI_API_URL}/auth/local`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ identifier: email, password: password }),
-      });
-
-      const data = await response.json();
-
-      if (data.jwt) {
-        // تخزين التوكن في HTTP-Only Cookie
-        res.setHeader(
-          'Set-Cookie',
-          serialize('token', data.jwt, {
-            httpOnly: true,  // الجافاسكريبت مش هتقدر تقرأ الكوكيز
-            secure: process.env.NODE_ENV === 'production', // تشغيل HTTPS في بيئة الإنتاج
-            sameSite: 'Strict',
-            path: '/',
-            maxAge: 60 * 60 * 24 * 7,  // مدة الكوكيز: 7 أيام
-          })
-        );
-
-        res.status(200).json({ message: 'Logged in successfully' });
-      } else {
-        res.status(400).json({ message: 'Invalid credentials' });
-      }
-    } catch (error) {
-      res.status(500).json({ message: 'Server error' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
   }
+
+  // هنا تقدر تستخدم التوكن لعمل أي تحقق أو استعلام من Strapi
+  return {
+    props: { token },
+  };
 }
--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -3,6 +3,8 @@ import axios from "axios";
 
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
 const apiUrl = "http://localhost:1337";
+console.log('saed',process.env.STRAPI_API_URL);
+
 
 const axiosClient = axios.create({
   baseURL: apiUrl,
