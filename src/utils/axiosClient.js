e7d4f5af2c85a54d831cce156ea21c0b770cd326
saed abbas <saedabbas0@gmail.com>
2025-04-29 15:18:17 +0300

    header

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
+const apiKey = process.env.NEXT_PUBLIC_STRAPI_TOKEN;
 const apiUrl = process.env.NEXT_PUBLIC_STRAPI_API_URL;
   
 
 const axiosClient = axios.create({
   baseURL: apiUrl,
+  headers: {
+    Authorization: `Bearer ${apiKey}`,
+  },
 
 });
 
